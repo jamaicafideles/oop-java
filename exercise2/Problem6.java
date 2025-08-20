bd1e7f74c30366444f69f7426962e3b1581c7d62
jamaicafideles <fidelesjamaica64@gmail.com>
2025-08-20 15:11:28 +0800

    Add files via upload

---
+++ exercise2/Problem6.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Problem6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter range: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " - " + (num * i));
+        }
+
+    }
+
+}
